b619ac758613c39b2d9acb7614ba4541f8aaa0d5
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2019-06-15 18:31:47 -0400

    Added new KodiUpdate scripts

---
+++ KodiUpdateTVShow.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+UserPassword="kodi:TheFlyingFish"
+Host=http://192.168.1.2:2020/jsonrpc
+
+x=300
+
+while [ $x -le 304 ]
+do
+  echo "Welcome $x times"
+  x=$(( $x + 1 ))
+
+GenerateJsonData()
+{
+  cat <<EOF
+{
+   "id":1,
+   "jsonrpc":"2.0",
+   "method":"VideoLibrary.RefreshTVShow",
+   "params":{
+     "tvshowid": $x,
+     "ignorenfo": true,
+     "refreshepisodes": true
+    }
+}
+EOF
+}
+
+curl -v -X POST \
+-H "Content-Type:application/json" \
+-d "$(GenerateJsonData)" \
+-u $UserPassword $Host
+
+done
+
+echo "\n"
+echo "First arg. is" $x
